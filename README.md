a1e2fe5eef19cd33bf9f2baf8df6a4d5d6ec15d2
Nathan Willson <willson.b.nathan@gmail.com>
2017-02-17 16:56:20 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# configs
+# Configs
 My configs
+
+Reference: https://github.com/syotaro/dotfiles
+
+Run this
+```
+cd ~
+git clone git@github.com:sabasis/configs.git
+cd ~/configs
+sh ./deploy-dotfiles-all.sh
+```
---
+++ deploy-dotfiles-all.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd $(dirname $0)
+for dotfile in .?*
+do
+    if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
+    then
+        ln -Fis "$PWD/$dotfile" $HOME
+    fi
+done
+
+#ln -s ~/Dropbox/work/database ~/database
+#ln -s ~/Dropbox/work/svn ~/svn
+#ln -s ~/Dropbox/work/github ~/github
+#ln -s ~/Dropbox/work/github/dotfiles ~/github/dotfiles
+#
